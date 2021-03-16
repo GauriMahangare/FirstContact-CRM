decb40932942a24b751c186a82c22c0bee439bae
Gauri Mahangare <gauri.mahangare@avanengineering.com>
2021-03-16 18:44:37 +0000

    Updated settings file to include default host

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["firstcontactcrm.avanengineering.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["firstcontactcrm.avanengineering.com","intense-castle-30858.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
